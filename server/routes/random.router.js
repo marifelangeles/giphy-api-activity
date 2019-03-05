797d6c0fa5992985680c7a9636de34b2bbd65910
Marifel Angeles <mmarifel@gmail.com>
2019-03-05 16:29:42 -0600

    Make a `GET` request to Giphy and send response to client

--- server/routes/random.router.js
+++ server/routes/random.router.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router.get('/', (req, res) => {
-    res.send('Hello World'); // Replace this
-})
+    // Make a `GET` request to Giphy
+    axios({
+        method: 'GET',
+        url: `http://api.giphy.com/v1/gifs/random`,
+        params: {
+            key: process.env.GIPHY_API_KEY,
+        }
+    }).then( response => {
+        console.log('response', response.data);
+        res.send(response.data);
+    }).catch(error => {
+        console.log('error with GET', error);
+    });
+});
 
 module.exports = router;
